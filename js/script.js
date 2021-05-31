ddbe636100fbae3cae23a380967775432b82a46b
IgormBonfim <igormbonfim@hotmail.com>
2021-05-31 16:32:43 -0300

    Header adicionado

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+document.querySelectorAll("a.paginasmenu").forEach(link => {
+    const conteudo = document.getElementById("conteudo")
+
+    link.onclick = function(e) {
+        e.preventDefault()
+        fetch(link.href)
+            .then(resp => resp.text())
+            .then(html => conteudo.innerHTML = html)
+    }
+})
