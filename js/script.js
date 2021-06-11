aafd3e575b59c24dda989e637209c14eadec571f
IgormBonfim <igormbonfim@hotmail.com>
2021-06-10 22:09:57 -0300

    Projeto - Bot Joshue finalizado

--- js/script.js
+++ js/script.js
@@ -9,3 +9,14 @@ document.querySelectorAll("a.paginasmenu").forEach(link => {
             .then(html => conteudo.innerHTML = html)
     }
 })
+
+document.querySelectorAll("a#voltar").forEach(link => {
+    const conteudo = document.getElementById("conteudo")
+
+    link.onclick = function(e) {
+        e.preventDefault()
+        fetch(link.href)
+            .then(resp => resp.text())
+            .then(html => conteudo.innerHTML = html)
+    }
+})
