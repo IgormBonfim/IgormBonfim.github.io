3c4629c9830e7a26d1c5e92280bbcebde0269cfa
Igor Bonfim <igormbonfim@hotmail.com>
2022-03-23 21:07:56 -0300

    Projeto BotJoshue finalizado

--- js/script.js
+++ js/script.js
@@ -1,22 +1,2 @@
-
-document.querySelectorAll("a.paginasmenu").forEach(link => {
-    const conteudo = document.getElementById("conteudo")
-
-    link.onclick = function(e) {
-        e.preventDefault()
-        fetch(link.href)
-            .then(resp => resp.text())
-            .then(html => conteudo.innerHTML = html)
-    }
-})
-
-document.querySelectorAll("a#voltar").forEach(link => {
-    const conteudo = document.getElementById("conteudo")
-
-    link.onclick = function(e) {
-        e.preventDefault()
-        fetch(link.href)
-            .then(resp => resp.text())
-            .then(html => conteudo.innerHTML = html)
-    }
-})
+idade = new Date()
+console.log(idade)
