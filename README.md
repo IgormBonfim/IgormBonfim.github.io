93121031c62d0af0dfc7521aaf633bd84d065d11
Igor Bonfim <igormbonfim@hotmail.com>
2023-11-17 02:22:28 -0300

    Atualiza experiencias e projetos

--- README.md
+++
@@ -1,2 +0,0 @@
-# Igormbonfim.github.io
-### Apenas um site pessoal, com informações sobre mim e meus projetos.
---
+++ angular/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  curriculoUrl: 'https://drive.google.com/file/d/1oEgmVe2iLgA6UpmbK6PjT8Lhe5dZ9Tgy/view',
+  resumeUrl: 'https://drive.google.com/file/d/1DQgbUpToUJJPEBp9la583bOfxNSKbY_Y/view?usp=drive_link'
+};
