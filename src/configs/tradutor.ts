67e1165376bbf95df103abd0e4e88a858e0b9f38
Igor Bonfim <igormeirabonfim@gmail.com>
2024-08-26 01:15:18 -0300

    Fix: Ajusta import de lingaguens

--- src/configs/tradutor.ts
+++ src/configs/tradutor.ts
@@ -1,7 +1,7 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import ptBRJSON from '.././assets/languages/ptBr.json'
-import enUSJSON from '.././assets/languages/enUs.json'
+import ptBRJSON from '.././assets/languages/ptBR.json'
+import enUSJSON from '.././assets/languages/enUS.json'
 
 const init = localStorage.getItem("linguagem");
 
