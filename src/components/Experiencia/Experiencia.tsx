826f608f5286b551865e3afc2e8aa375c41a5d77
Igor Bonfim <igormeirabonfim@gmail.com>
2024-08-26 23:31:37 -0300

    Feat: implementa motion nas sections sobre e experiencias

--- src/components/Experiencia/Experiencia.tsx
+++ src/components/Experiencia/Experiencia.tsx
@@ -1,26 +1,40 @@
-import { useTranslation } from "react-i18next"
+import { useTranslation } from "react-i18next";
 import { Experiencia as ExperienciaType } from "../../types/experienca";
 import TecnologiaTag from "../TecnologiaTag/TecnologiaTag";
+import { motion } from "framer-motion";
 
 export default function Experiencia(experiencia: ExperienciaType) {
-
-    const { t } = useTranslation();
-    return (
-        <article className="mb-8 flex flex-wrap lg:justify-center ]">
-            <div className="w-full lg:w-1/4">
-                <p className="mb-2 text-sm text-neutral-400">{t(experiencia.ano)}</p>
-            </div>
-            <div className="w-full max-w-xl lg:w-3/4">
-                <h6 className="mb-1 font-semibold"> {t(experiencia.cargo)}
-                </h6>
-                    <span className="text-sm font-bold text-blue-800">
-                        {t(experiencia.empresa)}
-                    </span>
-                <p className="mb-4 text-neutral-400 text-xs sm:text-base text-justify"> {t(experiencia.descricao)} </p>
-                <div className="flex flex-wrap gap-2">
-                    {experiencia.tecnologias.map((tecnologia, index) => (<TecnologiaTag key={index} {...{tecnologia}}/>))}
-                </div>
-            </div>
-        </article>
-    )
+  const { t } = useTranslation();
+  return (
+    <article className="mb-8 flex flex-wrap lg:justify-center ]">
+      <motion.div
+        className="w-full lg:w-1/4"
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 1 }}
+      >
+        <p className="mb-2 text-sm text-neutral-400">{t(experiencia.ano)}</p>
+      </motion.div>
+      <motion.div
+        className="w-full max-w-xl lg:w-3/4"
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: 100 }}
+        transition={{ duration: 1 }}
+      >
+        <h6 className="mb-1 font-semibold"> {t(experiencia.cargo)}</h6>
+        <span className="text-sm font-bold text-blue-800">
+          {t(experiencia.empresa)}
+        </span>
+        <p className="mb-4 text-neutral-400 text-xs sm:text-base text-justify">
+          {" "}
+          {t(experiencia.descricao)}{" "}
+        </p>
+        <div className="flex flex-wrap gap-2">
+          {experiencia.tecnologias.map((tecnologia, index) => (
+            <TecnologiaTag key={index} {...{ tecnologia }} />
+          ))}
+        </div>
+      </motion.div>
+    </article>
+  );
 }
--- src/components/Sobre/Sobre.tsx
+++ src/components/Sobre/Sobre.tsx
@@ -1,27 +1,73 @@
-import { useTranslation } from "react-i18next"
+import { motion } from "framer-motion";
+import { useTranslation } from "react-i18next";
 
 export default function Sobre() {
-    const IMG_URL = "https://github.com/igormbonfim.png";
-    const CARGO = "cargo";
-    const SOBRE = "sobre";
-    const URL_CURRICULO = "linkCurriculo";
-    const URL_WHATSAPP = "https://api.whatsapp.com/send?phone=5527992523204";
-    const BTN_CONTATO = "btn.contato";
+  const IMG_URL = "https://github.com/igormbonfim.png";
+  const CARGO = "cargo";
+  const SOBRE = "sobre";
+  const URL_CURRICULO = "linkCurriculo";
+  const URL_WHATSAPP = "https://api.whatsapp.com/send?phone=5527992523204";
+  const BTN_CONTATO = "btn.contato";
 
-    const { t } = useTranslation();
-    return (
-        <section className="border-b border-neutral-900 pb-4 lg:mb-36">
-            <div className="flex flex-col items-center gap-4">
-                <img src={IMG_URL} alt="Igor Bonfim - Desenvolvedor de Software Full-Stack" className="rounded-full max-w-64"/>
-                <h2 className="text-3xl">{t(CARGO)}</h2>
-                <p className="my-2 max-w-xl py-4 font-light text-justify">
-                    {t(SOBRE)}
-                </p>
-                <div className="flex justify-center gap-4 text-lg">
-                        <a href={URL_WHATSAPP} target="_blank" className="rounded-xl bg-blue-700 hover:bg-blue-600 p-4">{t(BTN_CONTATO)}</a>
-                    <a href={t(URL_CURRICULO)} target="_blank" className="rounded-xl border border-blue-700 text-blue-700 hover:text-zinc-100 hover:bg-blue-700 p-4">Download CV</a>
-                </div>
-            </div>
-        </section>
-    )
+  const container = (delay: number) => ({
+    hidden: { x: -100, opacity: 0 },
+    visible: {
+      x: 0,
+      opacity: 1,
+      transition: { duration: 0.5, delay: delay },
+    },
+  });
+
+  const { t } = useTranslation();
+  return (
+    <section className="border-b border-neutral-900 pb-4 lg:mb-36">
+      <div className="flex flex-col items-center gap-4">
+        <motion.img
+          src={IMG_URL}
+          alt="Igor Bonfim - Desenvolvedor de Software Full-Stack"
+          className="rounded-full max-w-64"
+          initial={{ x: 100, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 1, delay: 0 }}
+        />
+        <motion.h2
+          variants={container(0.5)}
+          initial="hidden"
+          animate="visible"
+          className="text-3xl"
+        >
+          {t(CARGO)}
+        </motion.h2>
+        <motion.p
+          variants={container(1)}
+          initial="hidden"
+          animate="visible"
+          className="my-2 max-w-xl py-4 font-light text-justify"
+        >
+          {t(SOBRE)}
+        </motion.p>
+        <motion.div
+          variants={container(1)}
+          initial="hidden"
+          animate="visible"
+          className="flex justify-center gap-4 text-lg"
+        >
+          <a
+            href={URL_WHATSAPP}
+            target="_blank"
+            className="rounded-xl bg-blue-700 hover:bg-blue-600 p-4"
+          >
+            {t(BTN_CONTATO)}
+          </a>
+          <a
+            href={t(URL_CURRICULO)}
+            target="_blank"
+            className="rounded-xl border border-blue-700 text-blue-700 hover:text-zinc-100 hover:bg-blue-700 p-4"
+          >
+            Download CV
+          </a>
+        </motion.div>
+      </div>
+    </section>
+  );
 }
